d48febd8ff4a83a88f36cf6ba4b06fcffcb36061
majidhussain1op <142897507+majidhussain1op@users.noreply.github.com>
2025-02-08 19:32:08 +0500

    Add files via upload

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,12 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+
+import { categoryType } from './categoryType'
+import { salesType } from './salesType'
+import { productType } from './productType'
+
+
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [ categoryType,salesType,productType],
+}
